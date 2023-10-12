82dd9c063541d0d2d1025297dbcd25fc2fd2f68e
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-12 19:47:56 -0300

    Create IBaseRepository.cs

---
+++ PerinityDesafio.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿using PerinityDesafio.Domain.Entities;
+
+namespace PerinityDesafio.Domain.Interfaces;
+
+public interface IBaseRepository<T> where T : BaseEntity 
+{
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> GetAllAsync();
+}
