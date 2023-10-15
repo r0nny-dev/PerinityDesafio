4bcb274673e6d62efb958b87d2b50079a09f0b91
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-15 07:58:27 -0300

    Criação do Contexto

---
+++ PerinityDesafio.Infrastructure/Context/DatabaseContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PerinityDesafio.Domain.Entities;
+
+namespace PerinityDesafio.Infrastructure.Context;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    public DbSet<PersonRegister> Persons { get; set; }
+    public DbSet<TaskRegister> TaskRegisters { get; set; }
+    public DbSet<DepartmentRegister> Departments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TaskRegister>()
+            .HasKey(tk => tk.Id);
+
+        modelBuilder.Entity<PersonRegister>()
+            .HasKey(ps => ps.Id);
+
+        modelBuilder.Entity<DepartmentRegister>()
+            .HasKey(dp => dp.Id);
+
+
+        modelBuilder.Entity<TaskRegister>()
+            .HasOne(tk => tk.PersonRegister)
+            .WithMany(ps => ps.TaskRegisters)
+            .HasForeignKey(tk => tk.PersonRegisterId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(false);
+
+        modelBuilder.Entity<TaskRegister>()
+            .HasOne(tk => tk.DepartmentRegister)
+            .WithMany(dp => dp.TaskRegisters)
+            .HasForeignKey(tk => tk.DepartmentRegisterId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        modelBuilder.Entity<PersonRegister>()
+            .HasOne(ps => ps.DepartmentRegister)
+            .WithMany(dp => dp.PersonRegisters)
+            .HasForeignKey(ps => ps.DepartmentRegisterId)
+            .IsRequired();
+    }
+}
