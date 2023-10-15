f16be2b573a50741fa3408d45cb4de7d52fe5c97
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-15 07:58:57 -0300

    Criação do Metodo CinfigurePersistenceAPP

---
+++ PerinityDesafio.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PerinityDesafio.Domain.Interfaces;
+using PerinityDesafio.Infrastructure.Context;
+using PerinityDesafio.Infrastructure.Repositories;
+
+namespace PerinityDesafio.Infrastructure;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistenceApp(this IServiceCollection services, IConfiguration configuration)
+    {
+        var ConnectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<DatabaseContext>(opt => opt.UseSqlServer(ConnectionString));
+
+        services.AddScoped<IPersonRepository, PersonRepository>();
+        services.AddScoped<IDepartmentRepository, DepartmantRepository>();
+        services.AddScoped<ITaskRepository, TaskRepository>();
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+    }
+}
