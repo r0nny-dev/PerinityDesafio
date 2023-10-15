8324fe54b54cd013f78b8afa172a9372964a0056
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-15 07:59:26 -0300

    Criação e Implementação do UnitOfWork

---
+++ PerinityDesafio.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using PerinityDesafio.Domain.Interfaces;
+using PerinityDesafio.Infrastructure.Context;
+
+namespace PerinityDesafio.Infrastructure.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DatabaseContext _context;
+    
+    public UnitOfWork(DatabaseContext Context) 
+    {
+        _context = Context;
+    }
+
+    public async Task Commit()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
