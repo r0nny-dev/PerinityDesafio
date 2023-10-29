28cdc7a5c4df9c1c783850f3114556831581c5b2
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-29 11:34:50 -0300

    Update PersonRepository.cs

--- PerinityDesafio.Infrastructure/Repositories/PersonRepository.cs
+++ PerinityDesafio.Infrastructure/Repositories/PersonRepository.cs
@@ -1,4 +1,6 @@
-﻿using PerinityDesafio.Domain.Entities;
+﻿
+using Microsoft.EntityFrameworkCore;
+using PerinityDesafio.Domain.Entities;
 using PerinityDesafio.Domain.Interfaces;
 using PerinityDesafio.Infrastructure.Context;
 
@@ -6,6 +8,18 @@ namespace PerinityDesafio.Infrastructure.Repositories;
 
 public class PersonRepository : BaseRepository<PersonRegister>, IPersonRepository
 {
+    private readonly DatabaseContext _context;
+
     public PersonRepository(DatabaseContext context) : base(context)
-    {}
+    {
+        _context = context;
+    }
+
+    public async Task<List<PersonRegister>> GetAllPerson(CancellationToken cancellationToken)
+    {
+        return await _context.Persons
+            .Include(ps => ps.DepartmentRegister)
+            .Include(ps => ps.TaskRegisters)
+            .ToListAsync();
+    }
 }
