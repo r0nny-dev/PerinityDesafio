d907843534e54113531a2bb4824dc55222626705
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-29 17:59:32 -0300

    Atualização do IDepartmentRepository e DepartmentRepository

--- PerinityDesafio.Domain/Interfaces/IDepartmentRepository.cs
+++ PerinityDesafio.Domain/Interfaces/IDepartmentRepository.cs
@@ -4,4 +4,5 @@ namespace PerinityDesafio.Domain.Interfaces;
 
 public interface IDepartmentRepository : IBaseRepository<DepartmentRegister>
 {
+    Task<List<DepartmentRegister>> GetDepartments(CancellationToken cancellationToken);
 }
--- PerinityDesafio.Infrastructure/Repositories/DepartmantRepository.cs
+++ PerinityDesafio.Infrastructure/Repositories/DepartmantRepository.cs
@@ -1,4 +1,5 @@
-﻿using PerinityDesafio.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PerinityDesafio.Domain.Entities;
 using PerinityDesafio.Domain.Interfaces;
 using PerinityDesafio.Infrastructure.Context;
 
@@ -6,6 +7,16 @@ namespace PerinityDesafio.Infrastructure.Repositories;
 
 public class DepartmantRepository : BaseRepository<DepartmentRegister>, IDepartmentRepository
 {
+    private readonly DatabaseContext _context;
+
     public DepartmantRepository(DatabaseContext context) : base(context)
-    {}
+    {
+        _context = context;
+    }
+
+    public async Task<List<DepartmentRegister>> GetDepartments(CancellationToken cancellationToken)
+        => await _context.Departments
+                    .Include(dp => dp.PersonRegisters)
+                    .Include(dp => dp.TaskRegisters)    
+                    .ToListAsync();
 }
