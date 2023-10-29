79cc81d1803add814fdaa1470ad072bb7627ef0d
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-29 11:38:09 -0300

    Update CreateTaskResponse.cs

--- PerinityDesafio.Application/UseCases/CreateTask/CreateTaskResponse.cs
+++ PerinityDesafio.Application/UseCases/CreateTask/CreateTaskResponse.cs
@@ -3,6 +3,7 @@
 public sealed record CreateTaskResponse
 {
     public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public DateTime Deadline { get; set; }
     public short Duration { get; set; }
