906b1348c6970cb4c68dc60b7920af5d87283754
Ronald Oliveira <97353520+r0nny-dev@users.noreply.github.com>
2023-10-29 11:34:18 -0300

    Update TaskRepository.cs

--- PerinityDesafio.Infrastructure/Repositories/TaskRepository.cs
+++ PerinityDesafio.Infrastructure/Repositories/TaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using PerinityDesafio.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PerinityDesafio.Domain.Entities;
 using PerinityDesafio.Domain.Interfaces;
 using PerinityDesafio.Infrastructure.Context;
 
@@ -6,7 +7,17 @@ namespace PerinityDesafio.Infrastructure.Repositories;
 
 public class TaskRepository : BaseRepository<TaskRegister>, ITaskRepository
 {
+    private readonly DatabaseContext _databaseContext;
+
     public TaskRepository(DatabaseContext context) : base(context)
     {
+        _databaseContext = context;
     }
+
+    public async Task<TaskRegister?> GetByIdTask(long id)
+        => await _databaseContext.TaskRegisters
+                .Include(tk => tk.PersonRegister)
+                .Include(tk => tk.DepartmentRegister)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
 }
